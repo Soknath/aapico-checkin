efc4bc634d11a558c587ba508d02dc3277645816
MIL Soknath <milsoknath@MILs-MacBook-Pro.local>
2020-04-17 11:54:29 +0700

    Updates

--- precache-manifest.2ec922f19a3a86cbc2c43c1d6bb2764d.js
+++ precache-manifest.2ec922f19a3a86cbc2c43c1d6bb2764d.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/aapico-checkin/static/js/runtime~main.e05235ce.js"
   },
   {
-    "revision": "6fd1cf8d8b13203735c5",
-    "url": "/aapico-checkin/static/js/main.6fd1cf8d.chunk.js"
+    "revision": "6feedceb1cea87758df0",
+    "url": "/aapico-checkin/static/js/main.6feedceb.chunk.js"
   },
   {
     "revision": "22eeb6ccfba576c7e61f",
     "url": "/aapico-checkin/static/js/2.22eeb6cc.chunk.js"
   },
   {
-    "revision": "6fd1cf8d8b13203735c5",
+    "revision": "6feedceb1cea87758df0",
     "url": "/aapico-checkin/static/css/main.aac1b552.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/aapico-checkin/static/css/2.faa3af53.chunk.css"
   },
   {
-    "revision": "79578709e6475dd260081170e2809c29",
+    "revision": "9d5291a135e65cdfdbe3c8e4f7ec724b",
     "url": "/aapico-checkin/index.html"
   }
 ];
