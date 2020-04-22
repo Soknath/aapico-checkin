65f6845b9bf4d01a81b90f850736a322806dec50
MIL Soknath <milsoknath@MILs-MacBook-Pro.local>
2020-04-23 02:19:34 +0700

    Updates

---
+++ precache-manifest.01f5f3f23bc68f5166f3ff3c5a417711.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e05235ce3d2cd4b5ef63",
+    "url": "/aapico-checkin/static/js/runtime~main.e05235ce.js"
+  },
+  {
+    "revision": "f5584fb3921731a4c5b8",
+    "url": "/aapico-checkin/static/js/main.f5584fb3.chunk.js"
+  },
+  {
+    "revision": "b7f06f664c22206252a5",
+    "url": "/aapico-checkin/static/js/2.b7f06f66.chunk.js"
+  },
+  {
+    "revision": "f5584fb3921731a4c5b8",
+    "url": "/aapico-checkin/static/css/main.0375503d.chunk.css"
+  },
+  {
+    "revision": "f207b9211b4391d95bc6b113b325b36d",
+    "url": "/aapico-checkin/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/aapico-checkin/precache-manifest.4db384ddd54dec07b00dd14ce70e6a4b.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/aapico-checkin/precache-manifest.01f5f3f23bc68f5166f3ff3c5a417711.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 console.log("Worker is working")
 
